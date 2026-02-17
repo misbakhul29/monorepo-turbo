9785fe74e1b6a4f1d2d4189e30f41bd13188a58b
misbakhul29 <misbakhul2904@gmail.com>
2026-02-17 15:22:55 +0700

    feat: auth

---
+++ packages/auth/src/auth.ts
@@ -0,0 +1,19 @@
+import { prisma } from "@repo/database";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql", 
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  // Tambahkan social providers jika perlu
+  // socialProviders: {
+  //   google: {
+  //     clientId: process.env.GOOGLE_CLIENT_ID!,
+  //     clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+  //   },
+  // },
+});
--- packages/ui/src/components/ui/alert-dialog.tsx
+++ packages/ui/src/components/ui/alert-dialog.tsx
@@ -1,8 +1,8 @@
 import { AlertDialog as AlertDialogPrimitive } from "radix-ui"
 import * as React from "react"
 
-import { Button } from "@/src/components/ui/button"
 import { cn } from "../../lib/utils"
+import { Button } from "./button"
 
 function AlertDialog({
   ...props
